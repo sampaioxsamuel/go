f9b4cf62c51e05bf62b8460ca5b80ef63ce98dcb
Samuel <sampaioxsamuel@gmail.com>
2022-10-01 20:28:15 -0300

    Initial

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,js,svelte,ts}', './src/**/**/*.{html,js,svelte,ts}'],
+	theme: {
+		fontFamily: {
+			sans: [
+				'Inter',
+				'system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Open Sans, Helvetica Neue, sans-serif'
+			]
+		},
+		extend: {
+			boxShadow: {
+				default: '0 0 0 3px rgba(66, 153, 225, 0.6)',
+				dark: '0 0 0 3px rgb(135 135 135 / 60%)',
+				rose: '0 0 0 3px rgb(234 76 137 / 10%)'
+			}
+		}
+	},
+	plugins: []
+};
